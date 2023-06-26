1b8c26162bf64182a98962e94dac460ce64f3640
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-26 10:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,7 +22,34 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  const cartContainer = document.getElementById("cart");
+  const cart = getCart();
+
+  cartContainer.innerHTML = "";
+
+  if (cart.length === 0) {
+    cartContainer.innerHTML = "<p>No items in the cart</p>";
+    return;
+  }
+
+  const cartTotal = cart.reduce((total, item) => total + item.price, 0);
+
+  cart.forEach(item => {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.innerHTML = `
+      <span>${item.name}</span>
+      <span>$${item.price}</span>
+    `;
+
+    cartContainer.appendChild(cartItemElement);
+  });
+
+  const cartTotalElement = document.createElement("div");
+  cartTotalElement.innerHTML = `<strong>Total: $${cartTotal}</strong>`;
+  cartContainer.appendChild(cartTotalElement);
+}
+
 
 // Add item to cart
 function addToCart(productId) {}
@@ -34,5 +61,16 @@ function removeFromCart(productId) {}
 function clearCart() {}
 
 // Initial render
-renderProducts();
-renderCart();
+document.addEventListener("DOMContentLoaded", () => {
+  const addToCartButtons = document.getElementsByClassName("add-to-cart");
+
+  for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", handleAddToCart);
+  }
+
+  // Render products and cart when the page loads
+  renderProducts();
+  renderCart();
+});
+// renderProducts();
+// renderCart();
